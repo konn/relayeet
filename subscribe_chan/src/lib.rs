2fc0536d5ede6e1a30aeba580f20079fa8c58fb3
Hiromi ISHII <konn.jinro@gmail.com>
2018-09-04 01:23:51 +0900

    rebased on crossbeam_channel

--- subscribe_chan/src/lib.rs
+++ subscribe_chan/src/lib.rs
@@ -1,20 +1,22 @@
-use std::sync::mpsc;
+extern crate crossbeam_channel;
+use crossbeam_channel as chan;
 use std::sync::{Arc, Mutex, Weak};
 
 #[derive(Debug)]
 pub struct Sender<T> {
-    senders: Arc<Mutex<Vec<Weak<mpsc::Sender<T>>>>>,
+    senders: Arc<Mutex<Vec<Weak<chan::Sender<T>>>>>,
 }
 
 #[derive(Debug, Clone)]
 pub struct Subscriber<T> {
-    senders: Arc<Mutex<Vec<Weak<mpsc::Sender<T>>>>>,
+    senders: Arc<Mutex<Vec<Weak<chan::Sender<T>>>>>,
 }
 
+#[derive(Debug)]
 pub struct Receiver<T> {
-    receiver: mpsc::Receiver<T>,
+    receiver: chan::Receiver<T>,
     // We don't use _sender; just to make sure it dropped when the Rec'ver dropped
-    _sender: Arc<mpsc::Sender<T>>,
+    _sender: Arc<chan::Sender<T>>,
 }
 
 pub fn channel<T: Clone>() -> (Sender<T>, Subscriber<T>) {
@@ -33,13 +35,7 @@ impl<T: Clone> Sender<T> {
         *sends = sends
             .iter()
             .filter_map(|a| a.upgrade())
-            .filter_map(|s| {
-                if s.send(t.clone()).is_ok() {
-                    Some(s)
-                } else {
-                    None
-                }
-            })
+            .inspect(|s| s.send(t.clone()))
             .map(|a| Arc::downgrade(&a))
             .collect();
     }
@@ -47,7 +43,7 @@ impl<T: Clone> Sender<T> {
 
 impl<T> Subscriber<T> {
     pub fn subscribe(&self) -> Receiver<T> {
-        let (sender, receiver) = mpsc::channel();
+        let (sender, receiver) = chan::unbounded();
         let _sender = Arc::new(sender);
         let mut sends = self.senders.lock().unwrap();
         (*sends).push(Arc::downgrade(&_sender));
@@ -56,11 +52,11 @@ impl<T> Subscriber<T> {
 }
 
 impl<T> Receiver<T> {
-    pub fn recv(&self) -> Result<T, mpsc::RecvError> {
+    pub fn recv(&self) -> Option<T> {
         self.receiver.recv()
     }
 
-    pub fn try_recv(&self) -> Result<T, mpsc::TryRecvError> {
+    pub fn try_recv(&self) -> Option<T> {
         self.receiver.try_recv()
     }
 }
@@ -74,7 +70,7 @@ mod tests {
         let (s, p) = channel();
         s.send(123);
         let r = p.subscribe();
-        assert!(r.try_recv().is_err());
+        assert!(r.try_recv().is_none());
     }
 
     #[test]
@@ -83,10 +79,13 @@ mod tests {
         s.send(1);
         let r = p.subscribe();
         s.send(2);
-        assert_eq!(r.try_recv(), Ok(2));
-        let r2 = p.subscribe();
         s.send(3);
-        assert_eq!(r.try_recv(), Ok(3));
-        assert_eq!(r2.try_recv(), Ok(3));
+        assert_eq!(r.try_recv(), Some(2));
+        let r2 = p.subscribe();
+        s.send(4);
+        assert_eq!(r.try_recv(), Some(3));
+        assert_eq!(r2.try_recv(), Some(4));
+        assert_eq!(r.try_recv(), Some(4));
+        assert_eq!(r2.try_recv(), None);
     }
 }
