0b89fedddd424a55ac9795f9fab6702c63d0d3f9
Hiromi ISHII <konn.jinro@gmail.com>
2018-09-06 04:41:26 +0900

    Implements easy echoing client

--- app/client.hs
+++ app/client.hs
@@ -1,15 +1,24 @@
+{-# LANGUAGE OverloadedStrings, RecordWildCards #-}
 module Main where
+import Relayeet
 
-import Control.Concurrent
-import Control.Concurrent.STM
-import Control.Monad
-import System.Mem.Weak
-
-withFinalizer :: Integer -> IO (Weak Integer)
-withFinalizer i = mkWeakPtr i (Just $ putStrLn $ unwords ["released: ", show i])
+import           Conduit
+import           Data.Aeson.Parser
+import qualified Data.ByteString         as BS
+import           Data.Conduit.Attoparsec
+import           Network.HTTP.Conduit
+import           Network.HTTP.Types
 
 main :: IO ()
 main = do
-  tc <- newBroadcastTChanIO
-  forM_ [0 ..] $ atomically . writeTChan tc <=< withFinalizer
-  forever $ threadDelay maxBound
+  Right ClientConfig{..} <- parseClientArgs
+  let auth = (hAuthorization, "Bearer " <> getBearer bearer)
+      Just req0 = parseRequest url
+      req = req0 { requestHeaders = auth : requestHeaders req0 }
+  man <- newManager tlsManagerSettings
+  runResourceT $ do
+    src <- responseBody <$> http req man
+    runConduit $ src .| linesUnboundedAsciiC .| filterC (not . BS.null)
+                     -- .| conduitParser json
+                     -- .| mapC snd
+                     .| mapM_C (liftIO . print)
