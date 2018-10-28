7e033fb6a894e0f7d11c0b68ade522f1c8cb0536
Hiromi ISHII <konn.jinro@gmail.com>
2018-10-28 15:34:02 +0900

    Removes unecessary connecor

--- client-rs/src/main.rs
+++ client-rs/src/main.rs
@@ -7,11 +7,9 @@ extern crate serde_json;
 extern crate serde_yaml;
 extern crate tokio_serde_json;
 
-use actix::Actor;
-use actix_web::{actix, client, client::ClientConnector, HttpMessage};
+use actix_web::{actix, client, HttpMessage};
 use futures::future::Future;
 use futures::stream::Stream;
-use openssl::ssl::{SslConnector, SslMethod};
 use relayeet_client::*;
 use serde_json::Value;
 use std::env;
@@ -25,18 +23,13 @@ fn main() {
     let hauth = format!("Bearer {}", c.bearer);
 
     actix::run(|| {
-        let ssl_conn = SslConnector::builder(SslMethod::tls()).unwrap().build();
-        let conn = ClientConnector::with_connector(ssl_conn).start();
-
         client::get(c.url)
             .header("Authorization", hauth)
-            .with_connector(conn)
             .finish()
             .unwrap()
             .send()
             .map_err(|_| ())
             .and_then(|rsp| {
-                println!("{:?}", rsp);
                 ReadJson::<_, Value>::new(SerdeStream(rsp.payload()))
                     .for_each(|j| {
                         println!("{:?}", j);
